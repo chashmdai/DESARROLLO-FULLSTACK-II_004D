328770d97c1ed9a1802b5b6ee136f524e9d15bb7
chashmdai <torrejonsotob@gmail.com>
2025-09-23 20:12:19 -0300

    refactor(main): habilitar @EnableJpaRepositories y @EntityScan

--- src/main/java/cl/nexbyte/NexbyteApplication.java
+++ src/main/java/cl/nexbyte/NexbyteApplication.java
@@ -2,12 +2,14 @@ package cl.nexbyte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "cl.nexbyte.repository")
+@EntityScan(basePackages = "cl.nexbyte.model")
 public class NexbyteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(NexbyteApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(NexbyteApplication.class, args);
+  }
 }
