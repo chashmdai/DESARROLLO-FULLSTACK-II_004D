448fd3ab4a5cda8776c4f97857032231c352de63
chashmdai <torrejonsotob@gmail.com>
2025-09-23 16:18:59 -0300

    chore: esqueleto de bootstrap del backend (v4)

---
+++ src/main/java/cl/nexbyte/NexbyteApplication.java
@@ -0,0 +1,13 @@
+package cl.nexbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexbyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexbyteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/nexbyte/NexbyteApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.nexbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexbyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
