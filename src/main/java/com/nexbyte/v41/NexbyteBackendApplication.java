3edf6432ef64f940c42ea5b3798314b1a7b39e97
chashmdai <torrejonsotob@gmail.com>
2025-10-02 17:44:48 -0300

    chore(bootstrap): init Nexbyte v4.1.0 (Spring Boot 3.5.6)

---
+++ src/main/java/com/nexbyte/v41/NexbyteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexbyte.v41;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexbyteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexbyteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexbyte/v41/NexbyteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexbyte.v41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexbyteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
